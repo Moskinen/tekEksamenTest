7dac3ccdbffd4d978cc14864070475982873418a
Moskinen <mortenlyngsie@gmail.com>
2026-05-31 15:50:25 +0200

    Initial commit

---
+++ src/main/java/com/example/tekeksamentest/TekEksamenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tekeksamentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekEksamenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekEksamenTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tekeksamentest/TekEksamenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tekeksamentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekEksamenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
